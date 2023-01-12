e8177f27e9093b9bfe0218cebf0874b6304a7502
kellyjiang18 <99273147+kellyjiang18@users.noreply.github.com>
2023-01-12 13:30:01 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 /*Tools: code editor, browser, command line utility, 
 application and server utility, API platform
 */
+const express = require('express');
+const app = express();
+app.get('/',(req,res)=>{
+    res.send('This is the music app main page');
+});
 
+const genres = [
+    {name:'Pop'},
+    {name:'Hip Hop'},
+    {name:'Rap'},
+    {name:'Classical'},
+    {name:'Rock'},
+    {name:'Jazz'},
+    {name:'Blues'},
+    {name:'Electronic'},
+];
 //=========== ROUTES FOR HTTP GET REQUESTS ==========
+app.get('/api/genres',(req,res)=>{
+    res.send(genres);
+});
 
+app.get('/api/courses/:name',(req,res)=>{
+    const genre = genres.find(c=>c.name === (req.params.name));
+    if(!genre){
+        res.status(404).send("THe genre with the given name was not found");
+        return
+    }
+    res.send(genre);
+});
 
-
+app.listen(4000,()=>{
+    console.log('Listening on port 3000 ...')
+});
 
 //=========== ROUTES FOR HTTP POST REQUESTS ==========
 
